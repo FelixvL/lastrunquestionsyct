3ea55f53db22e2753357b182f2bcbe9e1dcbceec
FelixvL <digitaleservice@gmail.com>
2020-12-02 12:23:48 +0100

    GV

--- Demo.java
+++ Demo.java
@@ -1,22 +1,23 @@
 
-class Demo{    // GW
+class Demo{    // GV
 	public static void main(String [] args){
 		AAA b = new BBB();
-		b.ddd().ddd(); 
+		System.out.println(b.ddd().ddd()); 
 	}
 }
-
 class AAA{  
+	int getal = 14;
 	AAA ddd(){
 		System.out.println("aaa");
+		return new AAA();
 	}
 }
-
 class CCC extends AAA{  
+	int getal = 15;
 
 }
-
 class BBB extends CCC{ 
+	int getal = 16;
 	CCC ddd(){
 		System.out.println("ccc");
 		return new BBB();
