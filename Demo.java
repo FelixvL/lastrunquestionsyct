d53984eae5b1cba04c3624dfb111501131645059
FelixvL <digitaleservice@gmail.com>
2020-12-02 11:41:07 +0100

    TG

--- Demo.java
+++ Demo.java
@@ -1,12 +1,12 @@
 
-class Demo{    // TT
+class Demo{    // TG
 	public static void main(String [] args){
  		Geit dier = new Geit();
 		dier.rrr();  
 	}
 }
 interface RRR{
-	default void rrr(){
+	static void rrr(){
 		System.out.println("rrr");
 	}
 }
