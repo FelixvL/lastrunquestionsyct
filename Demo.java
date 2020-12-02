4bc4e5b16ef8b432053fd6fa969f1156c7d5b967
FelixvL <digitaleservice@gmail.com>
2020-12-02 11:16:36 +0100

    YZ uitleg

--- Demo.java
+++ Demo.java
@@ -5,15 +5,18 @@ class Demo{    // YZ
 		dier.lopen();
 	}
 }
-
 class Dier{
 	void lopen(){
 		System.out.println("lopen in dier");
 	}
 }
-
 class Geit extends Dier{
 	void lopen() throws Exception{ 
 		System.out.println("lopen in geit");
 	}
 }
+// 4 regels van overriding
+// returntype covariant
+// zelfde signature
+// access modifier niet vernauwen -(hetzelfde of wijder)
+// throws clausule covariant OF NIETS (checked exceptions) 
