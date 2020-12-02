bbd31effc482423f32202d3e3ae9f0b8d19c3a83
FelixvL <digitaleservice@gmail.com>
2020-12-02 11:20:56 +0100

    YA

--- Demo.java
+++ Demo.java
@@ -1,22 +1,17 @@
 
-class Demo{    // YZ
+class Demo{    // YA
 	public static void main(String [] args){
  		Dier dier = new Geit();
 		dier.lopen();
 	}
 }
 class Dier{
-	void lopen(){
+	void lopen() throws Exception{
 		System.out.println("lopen in dier");
 	}
 }
 class Geit extends Dier{
-	void lopen() throws Exception{ 
+	void lopen(){ 
 		System.out.println("lopen in geit");
 	}
 }
-// 4 regels van overriding
-// returntype covariant
-// zelfde signature
-// access modifier niet vernauwen -(hetzelfde of wijder)
-// throws clausule covariant OF NIETS (checked exceptions) 
