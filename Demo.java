5818557af5cf701cd06ded1ed59c9e450d6c2a90
FelixvL <digitaleservice@gmail.com>
2020-12-02 11:09:15 +0100

    YY

--- Demo.java
+++ Demo.java
@@ -1,16 +1,19 @@
 
-class Demo{    // YU
+class Demo{    // YY
 	public static void main(String [] args){
- 		System.out.println("hoi");
+ 		Dier dier = new Geit();
+		dier.lopen();
 	}
 }
 
 class Dier{
 	void lopen(){
+		System.out.println("lopen in dier");
 	}
 }
 
 class Geit extends Dier{
 	void lopen(int a) throws Exception{ 
+		System.out.println("lopen in geit");
 	}
 }
