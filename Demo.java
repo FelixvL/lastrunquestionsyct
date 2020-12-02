fd07bed86f30ec5ebd7d643f57002ee033df46f7
FelixvL <digitaleservice@gmail.com>
2020-12-02 11:29:16 +0100

    YA UITLEG AANDACHT

--- Demo.java
+++ Demo.java
@@ -1,12 +1,13 @@
 
 class Demo{    // YA
-	public static void main(String [] args){
+	public static void main(String [] args) throws{
  		Dier dier = new Geit();
-		dier.lopen();
+try
+		dier.lopen();  // afhandelen || throwsen
 	}
 }
 class Dier{
-	void lopen() throws Exception{
+	void lopen() throws Exception{   // declare
 		System.out.println("lopen in dier");
 	}
 }
@@ -15,3 +16,8 @@ class Geit extends Dier{
 		System.out.println("lopen in geit");
 	}
 }
+
+// inheritance - 4 regels van override
+// Exception regel
+	// exception throws mag ook 'gelogen' zijn
+	// checked exception - try catch OF throwsen || afhandelen of doorwerpen
