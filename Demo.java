f3b113fc1262546a1da8542e56c996c6028b701b
felixvl <digitaleservice@gmail.com>
2021-12-02 14:17:26 +0100

    UD

--- Demo.java
+++ Demo.java
@@ -1,10 +1,10 @@
 
-class Demo{    // UC
+class Demo{    // UD
 	public static void main(String [] args){
 		System.out.println("boe");
 		go();
 	}
-	void go(){
+	static public void go(){
 		System.out.println("boem");
 	}
 }
