fbcf7625b5c531564e63558cd02f38c093bcdd30
felixvl <digitaleservice@gmail.com>
2021-12-02 14:13:16 +0100

    UA

--- Demo.java
+++ Demo.java
@@ -1,26 +1,7 @@
 
-class Demo{    // GM
+class Demo{    // UA
 	public static void main(String [] args){
-		AAA b = new BBB();
-		System.out.println(b.ddd().ddd().getal); 
+		System.out.println("boe");
 	}
-}
-class AAA{  
-	int getal = 14;
-	CCC ddd(){
-		System.out.println("aaa");
-		return new AAA();
-	}
-}
-class CCC extends AAA{  
-	int getal = 15;
-
-}
-class BBB extends CCC{ 
-	int getal = 16;
-	CCC ddd(){
-		System.out.println("ccc");
-		return new BBB();
-	} 
 }
 
